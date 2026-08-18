891396ea5c10584bd00c8e7352474e49d87d15b1
chlbri <51156116+chlbri@users.noreply.github.com>
2026-08-18 08:38:38 +0000

    _NO_CI

--- src/ui/components/NodesBoard.tsx
+++ src/ui/components/NodesBoard.tsx
@@ -1,15 +1,11 @@
-import { useState } from '@bemedev/app-solidjs';
-import {
-  DragDropProvider,
-  DragDropSensors,
-  DragOverlay,
-} from '@thisbeyond/solid-dnd';
-import { dequal } from 'dequal';
-import { Component, createSignal, For, Show } from 'solid-js';
-import { DragBounds } from './Bounds';
-import { EdgesBoard } from './EdgesBoard';
-import { useFlow } from './FlowChart.context';
-import { NodeComponent } from './NodeComponent';
+import { useState } from "@bemedev/app-solidjs";
+import { DragDropProvider, DragDropSensors, DragOverlay } from "@thisbeyond/solid-dnd";
+import { dequal } from "dequal";
+import { Component, createSignal, For, Show } from "solid-js";
+import { DragBounds } from "./Bounds";
+import { EdgesBoard } from "./EdgesBoard";
+import { useFlow } from "./FlowChart.context";
+import { NodeComponent } from "./NodeComponent";
 
 export const NodesBoard: Component = () => {
   // const [ref, setRef] = createSignal<HTMLDivElement>();
@@ -20,19 +16,19 @@ export const NodesBoard: Component = () => {
   } = useFlow();
 
   const selectedId = useState(service, {
-    selector: s => s.context?.selected,
+    selector: (s) => s.context?.selected,
   });
   const selected = (id: string | number) => selectedId() === id;
 
   const nodes = useState(service, {
-    selector: s => {
+    selector: (s) => {
       const list = s.context?.data?.nodes ?? [];
-      return list.map(item => ({
+      return list.map((item) => ({
         id: item.id,
         x: item.position?.x ?? 0,
         y: item.position?.y ?? 0,
         label: item.data?.label,
-        content: item.data?.content ?? '',
+        content: item.data?.content ?? "",
         input: item.input ?? false,
       }));
     },
@@ -40,7 +36,7 @@ export const NodesBoard: Component = () => {
   });
 
   const [transform, setTransform] = createSignal({ x: 0, y: 0 });
-  const [id, setId] = createSignal<string | number>('');
+  const [id, setId] = createSignal<string | number>("");
 
   return (
     <DragDropProvider
@@ -51,7 +47,7 @@ export const NodesBoard: Component = () => {
           const X = node.offsetLeft + transform().x;
           const Y = node.offsetTop + transform().y;
           service.send({
-            type: 'MOVE_IMMEDIATE',
+            type: "MOVE_IMMEDIATE",
             payload: {
               id: `${id}`,
               x: X,
@@ -65,11 +61,11 @@ export const NodesBoard: Component = () => {
 
         const X = node.offsetLeft + transform().x;
         const Y = node.offsetTop + transform().y;
-        node.style.setProperty('top', Y + 'px');
-        node.style.setProperty('left', X + 'px');
+        node.style.setProperty("top", Y + "px");
+        node.style.setProperty("left", X + "px");
 
         service.send({
-          type: 'MOVE',
+          type: "MOVE",
           payload: {
             id: `${id}`,
             x: X,
@@ -79,13 +75,13 @@ export const NodesBoard: Component = () => {
         setTransform({ x: 0, y: 0 });
       }}
     >
-      <div class='w-[calc(100vw-32px)] h-[calc(100vh-64px)] border-2 border-gray-600 overflow-scroll rounded-lg mx-auto relative'>
+      <div class="w-[calc(100vw-32px)] h-[calc(100vh-64px)] border-2 border-gray-600 overflow-scroll rounded-lg mx-auto relative no-scrollbar">
         <DragDropSensors />
         <DragBounds />
         <div
           ref={setRef}
-          class='w-[350vw] h-[350vh] relative'
-          onMouseDown={() => service.send('DESELECT')}
+          class="w-[350vw] h-[350vh] relative"
+          onMouseDown={() => service.send("DESELECT")}
         >
           <EdgesBoard />
           <For each={nodes()} children={NodeComponent} />
@@ -93,7 +89,7 @@ export const NodesBoard: Component = () => {
       </div>
 
       <Show when={!id() || !selected(id())}>
-        <DragOverlay children='' />
+        <DragOverlay children="" />
       </Show>
     </DragDropProvider>
   );
